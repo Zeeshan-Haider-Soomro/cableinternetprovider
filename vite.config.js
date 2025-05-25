904344ff0c45da516508903214e63b573254a70d
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2025-05-25 11:30:22 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cableinternetprovider/',
+  // base: '/cableinternetprovider/',
   plugins: [react(),
         tailwindcss(),
-
   ],
+  base: "/", // Use "/" for custom domains or "/repo-name/" for default 
 })
